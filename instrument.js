c8b97da79c31f093bdc3ebf953a1d44f1cfd7cca
Nicolas Hrubec <nico.hrubec@sentry.io>
2026-03-06 15:34:50 +0100

    more data

--- instrument.js
+++ instrument.js
@@ -10,11 +10,14 @@ Sentry.init({
   beforeSendTransaction(event) {
     console.log("\n========== Transaction ==========");
     console.log("Transaction:", event.transaction);
+    console.log("Op:", event.contexts?.trace?.op);
+    console.log("Data:", JSON.stringify(event.contexts?.trace?.data, null, 2));
     console.log("Spans:");
     for (const span of event.spans || []) {
       console.log(
         `  [${span.op}] ${span.description} (${span.status || "ok"})`,
       );
+      if (span.data) console.log("    data:", JSON.stringify(span.data, null, 2));
     }
     console.log("=================================\n");
     // Send the event to Sentry
